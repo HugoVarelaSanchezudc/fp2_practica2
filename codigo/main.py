b645ee6b1152652835f1726163cde74d85742ec1
Hugo <hugo.varela.sanchez@udc.es>
2024-03-20 12:33:53 +0100

    Implementada logica del programa y guardada en una funcion mainç

--- codigo/main.py
+++ codigo/main.py
@@ -19,10 +19,13 @@ class Gestor_Colas:
     def proceso(self):
         return self._proceso
     
-    def is_penalitated(self, proceso, index, usuarios = []):
 
-        if (index == 1) or (index == 3):
-            tiempo_ejec = proceso.contador - proceso.tiempo_inicial
+    #Comprobar cosas
+
+
+    def is_penalitated(self, proceso, usuarios):
+            
+            tiempo_ejec = proceso.interaccion - proceso.tiempo_inicial
 
             if (tiempo_ejec > 5) and (proceso.user_id not in usuarios):
 
@@ -51,160 +54,129 @@ class Gestor_Colas:
                 colas[0].enqueue(i)
             
 
-archivo = sys.argv[1]
-cola_reg= aq.ArrayQueue()
-gpu_short = aq.ArrayQueue()
-gpu_long = aq.ArrayQueue()
-cpu_short = aq.ArrayQueue()
-cpu_long = aq.ArrayQueue()
-
-
-colas = (gpu_long,gpu_short,cpu_long,cpu_short)
-
-
-with open(archivo, 'r') as contenido:
-    info_procesos = contenido.read()
-    
-contador = 0
-
-#Creamos un for, dondem metemos cada linea de codigo de longitud distinta de 0 en la cola
-
-for line in info_procesos.split('\n'):
-
-    if len(line) != 0:
-
-        datos_proceso = line.split()
-        proces_id, user_id, tipo1, dur_estimada, dur_real = datos_proceso
-        proceso1=Procesos(proces_id, user_id, tipo1, dur_estimada, dur_real,contador)
-
-        cola_reg.enqueue(proceso1)
 
 
 
-usuarios_penalizados = [ ]
 
-while len(cola_reg) > 0:
-    proceso_actual = cola_reg.dequeue()
-    proceso_actual.tiempo_inicial = contador
-    proceso_actual.contador = contador
-    
-    proceso = Gestor_Colas(proceso_actual)
-    proceso.tipo_cola(colas)
-    
-    
-    # print('Usario\n',proceso_actual.user_id,'\n\n')
-    # print('Tipo',proceso_actual.d_estimada)
-    
-    # print(usuarios_penalizados)
-    
-    nombre = proceso_actual.user_id
+
+def penalizar(usuarios_penalizados, cola):
+    aux = cola.first()
+    nombre = aux.user_id
     
     if nombre in usuarios_penalizados:
 
-        if (proceso_actual.tipo == 'gpu') and (proceso_actual.d_estimada == 'short'):
+        if aux.tipo == 'gpu':
             
-            proceso_actual.d_estimada = 'long'
-            gpu_long.enqueue(gpu_short.dequeue())
-            usuarios_penalizados.pop(usuarios_penalizados.index(proceso_actual.user_id))
+            aux.d_estimada = 'long'
+            cola.enqueue(cola.dequeue())
+            usuarios_penalizados.pop(usuarios_penalizados.index(aux.user_id))
 
-            print(f'Movemos {proceso_actual.process_id} de {nombre} al final de la cola de gpu')
+            print(f'Movemos {aux.process_id} de {nombre} al final de la cola de gpu')
 
             
 
-        elif (proceso_actual.tipo == 'cpu') and (proceso_actual.d_estimada == 'short'):
+        elif aux.tipo == 'cpu':
 
-            proceso_actual.d_estimada = 'long'
-            cpu_long.enqueue(cpu_short.dequeue())
-            usuarios_penalizados.pop(usuarios_penalizados.index(proceso_actual.user_id))
+            aux.d_estimada = 'long'
+            cola.enqueue(cola.dequeue())
+            aux.pop(usuarios_penalizados.index(aux.user_id))
 
-            print(f'Movemos {proceso_actual.process_id} de {nombre} al final de la cola de cpu')
+            print(f'Movemos {aux.process_id} de {nombre} al final de la cola de cpu')
 
 
+
+
+
+def ejecucion (cola, usu_penalizados, contador):
+    aux = cola.first()
+
+    if aux.tiempo_inicial == None:
+
+        aux.tiempo_inicial = contador
+        aux.interaccion = contador
+        if aux.tipe == 'short':
+            penalizar(usu_penalizados, cola)
+    
     else:
-    
-        if len(gpu_short) > 0:
-            index = 1
-        elif len(cpu_short) > 0:
-            index = 3
-        elif len(gpu_long) > 0:
-            index = 0
+
+        if contador >= aux.tiempo_inicial + aux.d_real:
+            cola.dequeue()
+
         else:
-            index = 2
-            
-        while contador <= proceso_actual.tiempo_inicial - proceso_actual.contador:
-            contador += 1
-            proceso_actual.contador = contador
-
-
-        proceso.is_penalitated(proceso_actual,index, usuarios_penalizados)
-        
-        colas[index].dequeue()
-
+            if aux.tipo == 'short' :
+                aux.is_penalitated(aux, usu_penalizados)
+            aux.interaccion = contador
     
-    
-    
-   
-        
-
-    
-'''
-    print('cpu short', len(cpu_short))
-    print('cpu long', len(cpu_long))
-    print('gpu short', len(gpu_short))
-    print('gpu long', len(gpu_long))
-
-#H: pq no se mira ya la penalizacion antes de guardarlo, ya que al meterlo al tipo cola puede meter un proceso
-#que por la penalizacion iria en una cola distinta
-
-    
-        
-print('CPU Short\n\n\n', cpu_short)
-print('CPU Long\n\n\n', cpu_long)
-print('GPU Short\n\n\n', gpu_short)
-print('GPU Long\n\n\n', gpu_long)
-'''
 
 
 
 
 
+def main():
+
+    #archivo = sys.argv[1]
+    archivo = 'processes0.txt'
+
+
+    cola_reg= aq.ArrayQueue()
+    gpu_short = aq.ArrayQueue()
+    gpu_long = aq.ArrayQueue()
+    cpu_short = aq.ArrayQueue()
+    cpu_long = aq.ArrayQueue()
+
+
+    colas = (gpu_long,gpu_short,cpu_long,cpu_short)
+    usuarios_penalizados = []
+    contador = 0
+
+    with open(archivo, 'r') as contenido:
+        info_procesos = contenido.read()
+
+
+    #Creamos un for, dondem metemos cada linea de codigo de longitud distinta de 0 en la cola
+
+    for line in info_procesos.split('\n'):
+
+        if len(line) != 0:
+
+            datos_proceso = line.split()
+            proces_id, user_id, tipo1, dur_estimada, dur_real = datos_proceso
+            proceso1 = Procesos(proces_id, user_id, tipo1, dur_estimada, dur_real)
+
+            cola_reg.enqueue(proceso1)
+
+
+
+
+    while (len(cola_reg) + len(cpu_long) + len(cpu_short) + len(gpu_long) + len(gpu_short)) > 0:
+        proceso_actual = cola_reg.dequeue()
+
+        proceso = Gestor_Colas(proceso_actual)
+        proceso.tipo_cola(colas)
+
+
+        if len(cpu_short) > 0:
+            ejecucion(cpu_short, usuarios_penalizados, contador)
+
+
+        if len(gpu_short) > 0:
+            ejecucion(gpu_short, usuarios_penalizados, contador)
+
+
+        if len(cpu_long):
+            ejecucion(cpu_long, usuarios_penalizados, contador)
+
+
+        if len(gpu_long):
+            ejecucion(gpu_long, usuarios_penalizados, contador)
 
 
 
 
 
-'''
-#Pruebas
-
-print(cola_reg)
-procesos = Gestor_Colas(cola_reg) #Creo el gestor de colas
-print(procesos)
-procesos.tipo_cola(colas) #Asocio cada proceso de la cola de de registros segun su tipo y duracion en 4 colas diferentes
-
-print('Hola', cpu_short)'''
-
-
-#-------------------------------------------------------------------Pruebas--------------------------------------------------------------------------
+        contador += 1
 
 
 
-# print(cola_reg.__len__()) #Veo el tamaño total para luego comprobar que no hay perdidas
-
-# print(gpu_long.__len__(),gpu_short.__len__(),cpu_long.__len__(),cpu_short.__len__()) #Imprimo el tamaño de cada uno para comprobar que no hubo perdidas
-
-# #Imprimo el tipo y la duracion estimada de cada cola para comproabar que todo fue bien y no se "traspapeló nada"
-# while gpu_long.__len__() > 0:
-#     i = gpu_long.dequeue()
-#     print (i.tipo, i.d_estimada) 
-    
-# while gpu_short.__len__() > 0:
-#     i = gpu_short.dequeue()
-#     print (i.tipo, i.d_estimada) 
-
-# while cpu_long.__len__() > 0:
-#     i = cpu_long.dequeue()
-#     print (i.tipo, i.d_estimada) 
-    
-# while cpu_short.__len__() > 0:
-#     i = cpu_short.dequeue()
-#     print (i.tipo, i.d_estimada) 
+if __name__ == '__main__':
+    main()
--- codigo/procesos.py
+++ codigo/procesos.py
@@ -2,18 +2,18 @@ import array_queue as arr
 
 class Procesos:
 
-    def __init__(self,process_id, user_id,tipo,d_estimada,d_real,contador=0, tiempo_inicial = int()):
+    def __init__(self,process_id : str, user_id,tipo : str, d_estimada : int, d_real : int,interaccion = 0, tiempo_inicial = int()):
         self._user_id = user_id
         self._process_id = process_id
         self._tipo = tipo
         self._d_estimada = d_estimada
         self._d_real = d_real
-        self._contador = contador
+        self._interaccion = interaccion
         self._tiempo_inicial = tiempo_inicial
 
         
     def __str__(self) -> str:
-        return f'Contador: {self.contador}, {self.process_id}, {self.user_id}, {self.tipo}, {self.d_estimada}, d_real: {self.d_real}, d_inicial: {self.tiempo_inicial}'
+        return f'{self.process_id}, {self.user_id}, {self.tipo}, {self.d_estimada}, interaccion: {self.interaccion}, d_inicial: {self.tiempo_inicial}, d_real: {self.d_real}'
 
 
     @property
@@ -48,11 +48,11 @@ class Procesos:
         
 
     @property
-    def contador(self):
-        return self._contador
-    @contador.setter
-    def contador(self, contador):
-        self._contador = contador
+    def interaccion(self):
+        return self._interaccion
+    @interaccion.setter
+    def interaccion(self, interaccion):
+        self._interaccion = interaccion
 
 
     @property
